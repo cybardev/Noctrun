54499bc7214570bb5c0a8e03354059226cac8695
Sheikh <sheikh@cybar.dev>
2024-03-31 04:01:22 -0300

    Revert "Various updates"

--- Assets/Scripts/World/Despawner.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Despawner : MonoBehaviour
-{
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        Destroy(other.gameObject.transform.parent.gameObject);
-    }
-}
